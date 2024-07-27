2dc86d7020745a6804f3a58a26ddc86a88db4b79
SamSantaa <samuel@grayola.io>
2024-07-26 21:35:16 -0500

    Wordpress Installation

---
+++ wp-admin/upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirigiendo...</title>
+		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Fgrayola.io%2Fmakers%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D819&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Fgrayola.io%2Fmakers%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D819&reauth=1";
+		</script>
+
+		<p>Estás siendo redirigido a <a href="/wp-login.php?redirect_to=https%3A%2F%2Fgrayola.io%2Fmakers%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D819&reauth=1">/wp-login.php?redirect_to=https%3A%2F%2Fgrayola.io%2Fmakers%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D819&reauth=1</a></p>
+	</body>
+</html>
